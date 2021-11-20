8428f056178ec60648afa398f5cf4cbb14ee8ae9
mickeywise <michael.wilberforce@yahoo.com>
2021-11-20 13:40:56 +0300

    updates

---
+++ Pycharm_Projects/Flatmates Bill/main.py
@@ -0,0 +1,45 @@
+class Bill:
+    """
+    Objects that contains data about a bill. such as
+    total amount and period of the bill.
+    """
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    """
+    Create a flatmate person who lives in the flat
+    and pays a share of the bill
+    """
+
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount * weight
+        return  to_pay
+
+class PDFReport:
+    """
+    Creates a PDF file that contain data about the
+    flatmates such as their names, their due amount
+    and the period of the bill
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
+
+the_bill = Bill(amount = 120, period = "March 2021")
+john  = Flatmate(name="John", days_in_house=20)
+mary = Flatmate(name="Mary", days_in_house=25)
+
+print("John pays: ",john.pays(bill=the_bill, flatmate2=mary))
+print("Mary pays: ",mary.pays(bill=the_bill, flatmate2=john))
