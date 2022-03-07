f2a52df9b57d72c00726635947b34f48b3a35c7b
mickeywise <michael.wilberforce@yahoo.com>
2022-03-07 16:55:15 +0300

    Unversioned update

---
+++ py/if_elif_else.py
@@ -0,0 +1,6 @@
+grade = int(input("Key grade: "))
+if grade >= 75:
+    print("You passed!")
+else:
+    grade < 75
+    print("You failed")
---
+++ py/test.py
@@ -0,0 +1,4 @@
+programming_lang = ["Python", "Java", "C++"]
+#print(type(programming_lang))
+for lang in programming_lang:
+    print(lang)
